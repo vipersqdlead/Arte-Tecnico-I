081c62a0ea19e2fe9352a65a8ac6c3effb343a9f
vipersqdlead <168582527+vipersqdlead@users.noreply.github.com>
2025-04-15 14:50:57 -0400

    Tutorial: Upload Project

---
+++ Assets/TestAssets/Scripts/SimpleMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    Vector3 MoveDir;
+    public float moveSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void Move()
+    {
+        MoveDir.x =  Input.GetAxis("Horizontal");
+        MoveDir.z = Input.GetAxis("Vertical");
+        transform.position += Time.deltaTime * moveSpeed * MoveDir;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+}
